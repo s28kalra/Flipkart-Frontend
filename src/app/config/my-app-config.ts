7468a22f88d048ea5608cd510f7495a4c7c052fc
s28kalra <sagar.kalra@lntinfotech.com>
2022-01-26 22:45:09 +0530

    flipkart frontend

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa3lvbgoafTxK3WP5d7',
         issuer: 'https://dev-74229058.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://flipkart-shopp.herokuapp.com/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
